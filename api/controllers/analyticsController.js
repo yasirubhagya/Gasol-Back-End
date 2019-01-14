d1f71f60ed786c26ee4f9a4f1f424291caf53b5d
yasiru <yasirubhagya@gmail.com>
2019-01-14 19:41:10 +0530

    connection error

--- api/controllers/analyticsController.js
+++ api/controllers/analyticsController.js
@@ -1,11 +1,11 @@
 const connection = require('../../sqlconnection');
 exports.analytics_get_all = (req, res, next) => {
+    
     connection.query('SELECT * FROM CASH', function (error, results, fields) {
         if (error) {
             console.log(error);
         } else {
             console.log(results, fields);
-            connection.end();
             res.status(200).json(results);
         }
 
